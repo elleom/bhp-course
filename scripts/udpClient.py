febb9b356b567b4bb2a1588ba77e5c55ae170d8b
elleom <el_leo_m@hotmail.com>
2021-03-11 15:15:32 -0500

    fist commit

---
+++ scripts/udpClient.py
@@ -0,0 +1,16 @@
+import socket
+
+target_address_port = ("www.google.com", 80)
+
+# create UDP sockect
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# send
+message = str.encode("something")
+
+client.sendto(message, target_address_port)
+
+# receive
+data = client.recvfrom(4096)
+
+print(data)
